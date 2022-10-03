095e3e0908c65ac8ee9ab27451b253b0174caf1c
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-10-03 15:48:40 -0400

    UPDATE: Dark Legions Initial Commit of Game.

---
+++ desktop/src/com/darklegions/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.darklegions.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.darklegions.game.DarkLegions;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Dark_Legions");
+		new Lwjgl3Application(new DarkLegions(), config);
+	}
+}
