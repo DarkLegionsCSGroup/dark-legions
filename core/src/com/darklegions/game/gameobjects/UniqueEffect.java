a0ccf33940a315a38845e9872b1eab269e7c2046
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-10-29 13:57:44 -0400

    UPDATE: Fixed slight issue with classes and importing project assests

---
+++ core/src/com/darklegions/game/gameobjects/UniqueEffect.java
@@ -0,0 +1,5 @@
+package com.darklegions.game.gameobjects;
+
+public class UniqueEffect {
+
+}
--- core/src/com/darklegions/game/manager/GameManager.java
+++ core/src/com/darklegions/game/manager/GameManager.java
@@ -3,7 +3,7 @@ package com.darklegions.game.manager;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class GameManager {
-    public Player player1;
-    public Player player2;
+    public PlayerManager player1;
+    public PlayerManager player2;
     public Sprite spriteBackground;
 }
