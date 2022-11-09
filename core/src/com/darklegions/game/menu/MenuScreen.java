99304b970d25abfde0ae3647166b07e24014ab9f
Felix Chen <Felixx.Chenn@gmail.com>
2022-11-09 18:49:56 -0500

    FEAT: Add missing menu files

---
+++ core/src/com/darklegions/game/menu/MenuScreen.java
@@ -0,0 +1,102 @@
+package com.darklegions.game.menu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.darklegions.game.DarkLegions;
+
+import javax.swing.event.ChangeEvent;
+
+public class MenuScreen implements Screen {
+    private final Stage stage;
+    private final DarkLegions parent;
+
+    public MenuScreen(DarkLegions darkLegions) {
+        parent = darkLegions;
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void show() {
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+        Skin skin = new Skin(Gdx.files.internal("skin/star-soldier/skin/star-soldier-ui.json"));
+
+        TextButton newGame = new TextButton("New Game", skin);
+        TextButton options = new TextButton("Options", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+        table.add(newGame).fillX().uniformX();
+        table.row().pad(10, 0, 10, 0);
+        table.add(options).fillX().uniformX();
+        table.row();
+        table.add(exit).fillX().uniformX();
+
+        newGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.changeScreen(DarkLegions.APPLICATION);
+            }
+        });
+
+        options.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                parent.changeScreen(DarkLegions.OPTIONS);
+            }
+        });
+
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
