b8b400c161e384f59c0d262049c6db177d9ab6aa
JClark <jclarkgaming01@gmail.com>
2022-11-30 14:53:08 -0500

    feat: added 2 utilities classes

---
+++ core/src/com/darklegions/game/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.darklegions.game.utils;
+
+    /**
+     * This class is used to store all the constants used in the game
+     *
+    */
+public class Constants {
+    public static final float VIEWPORT_WIDTH = 5.0f;
+    public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+    public static final float VIEWPORT_HEIGHT = 5.0f;
+    public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+    public static final String TEXTURE_ATLAS_OBJECTS = "assets/skin.star-soldier/skin/star-soldier-ui.atlas";
+}
